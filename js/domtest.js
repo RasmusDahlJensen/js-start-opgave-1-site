905150c5a6ce232022dc0993ffaa09d327d5ea27
Rasmus Jensen <rasmusdj96@gmail.com>
2022-08-15 14:01:55 +0200

    Dom Javascript
    Har få ændringer til mit HTML også

---
+++ js/domtest.js
@@ -0,0 +1,75 @@
+let h1 = document.getElementById("headline").innerHTML
+console.log(h1);
+
+let h2 = document.getElementsByClassName("smallHeadline")
+console.log(h2[0].innerHTML);
+console.log(h2[1].innerHTML);
+
+let p = document.getElementById("paragraph").innerHTML
+console.log(p);
+
+let inputTitle = document.getElementById("inputTitle").innerHTML
+console.log(inputTitle);
+
+let monthLabel = document.getElementById("monthLabel").innerHTML
+console.log(monthLabel);
+
+let monthInput = document.getElementById("monthInput").value
+console.log(monthInput);
+
+let checkboxLabel = document.getElementById("checkboxLabel").innerHTML
+console.log(checkboxLabel);
+
+let checkbox = document.getElementById("checkbox").value
+console.log(checkbox);
+
+let colorLabel = document.getElementById("colorLabel").innerHTML
+console.log(colorLabel);
+
+let radioLabel = document.getElementById("radioLabel").innerHTML
+console.log(radioLabel);
+
+let radioLabel1 = document.getElementById("radioLabel1").innerHTML
+console.log(radioLabel1);
+
+let radioInput1 = document.getElementById("radioInput1").value
+console.log(radioInput1);
+
+let radioLabel2 = document.getElementById("radioLabel2").innerHTML
+console.log(radioLabel2);
+
+let radioInput2 = document.getElementById("radioInput2").value
+console.log(radioInput2);
+
+let radioLabel3 = document.getElementById("radioLabel3").innerHTML
+console.log(radioLabel3);
+
+let rangeLabel = document.getElementById("rangeLabel").innerHTML
+console.log(rangeLabel);
+
+let rangeInput = document.getElementById("rangeInput").value
+console.log(rangeInput);
+
+let buttonInput = document.getElementById("buttonInput").value
+console.log(buttonInput);
+
+let resetInput = document.getElementById("resetInput").value
+console.log(resetInput);
+
+let submitInput = document.getElementById("submitInput").value
+console.log(submitInput);
+
+let regButton = document.getElementById("regButton").innerHTML
+console.log(regButton);
+
+let selectLabel = document.getElementById("selectLabel").innerHTML
+console.log(selectLabel);
+
+let selectId = document.getElementById("selectId").value
+console.log(selectId);
+
+let optgroupClass = document.getElementsByClassName("optgroupClass").innerHTML
+console.log(optgroupClass);
+
+let optionClass = document.getElementsByClassName("optionClass").value
+console.log(optionClass);
