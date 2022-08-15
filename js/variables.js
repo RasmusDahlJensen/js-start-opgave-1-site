2b4bbc4fba25243924ff80ed464ff8f0b9a16b5e
Rasmus Jensen <rasmusdj96@gmail.com>
2022-08-15 10:32:06 +0200

    123

--- js/variables.js
+++ js/variables.js
@@ -12,5 +12,14 @@ const j = new Date();
 
 
 
-
+console.log("Variabel a er et " + typeof(a))
+console.log("Variabel b er et " + typeof(b))
+console.log("Variabel c er et " + typeof(c))
+console.log("Variabel d er et " + typeof(d))
+console.log("Variabel e er et " + typeof(e))
+console.log("Variabel f er et " + typeof(f))
+console.log("Variabel g er et " + typeof(g))
+console.log("Variabel h er et " + typeof(h))
+console.log("Variabel i er et " + typeof(i))
+console.log("Variabel j er et " + typeof(j))
 
